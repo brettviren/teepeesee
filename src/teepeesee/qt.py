de3a6dd923c06b41a633f34319386fe5d0bc093c
Brett Viren <brett.viren@gmail.com>
2026-01-19 09:46:53 -0500

    Remove old matplotlib based display and shorten cli entry point to qtpc.

--- src/teepeesee/qt.py
+++
@@ -1,9 +0,0 @@
-try:
-    import PyQt6 as PyQt
-    from PyQt6 import QtCore, QtGui, QtWidgets
-except ImportError:
-    import PyQt5 as PyQt
-    from PyQt5 import QtCore, QtGui, QtWidgets
-
-import pyqtgraph as pg
-
